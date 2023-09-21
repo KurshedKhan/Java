74d7f0d03ce7f06c138624abb11ba743b75653f0
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-09-21 22:37:51 +0530

    Print Hello World part 2 in java

---
+++ PrintHello2.java
@@ -0,0 +1,17 @@
+public class PrintHello2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World...!");
+        System.out.println("Good Morning...!");
+        System.out.println("Hi, How are you ?");
+
+
+        System.out.print("Namste ji ");
+        System.out.print("Java is a very easy language.\n");
+
+        System.out.print("a = 30 , b = 50 : a+b = ");
+        System.out.print(30 + 40);
+
+    }
+
+}
