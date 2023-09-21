a9d5fd008c009997594175de47909e1933271594
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-09-21 22:13:13 +0530

    Print Hello World in java

---
+++ PrintHello.java
@@ -0,0 +1,36 @@
+public class PrintHello {
+    /*  
+     *  Every line of code that runs in Java must be inside a class.
+     * 
+     *  The name of the java file must match the class name. When saving
+     *  the file, save it using the class name and add ".java" to the end of the filename.
+     * 
+     *  A class should always start with an uppercase first letter.
+     * 
+     *  Every Java program has a class name which must match the filename.
+     *  
+     *  Note: Java is case-sensitive: "MyClass" and "myclass" has different meaning.
+     * 
+     *  Note: The curly braces {} marks the beginning and the end of a block of code.
+     * 
+     *  main()==> Every program must contain the main() method.
+     * 
+     *  System ==>System is a built-in Java class that contains useful members,
+     *               such as out, which is short for "output".
+     * 
+     *  println ==> The println() method, short for "print line", is used to
+     *               print a value to the screen.
+     * 
+     *  There is also a print() method, which is similar to println().
+     *  The only difference is that it does not insert a new line at the end of the output:     
+     * 
+     *  You should also note that each code statement must end with a semicolon (;).
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World...!");
+
+    }
+
+}
