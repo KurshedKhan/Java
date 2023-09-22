7825b42fc0ff06f244ba7223afc5e5002e788612
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-09-22 23:03:49 +0530

    Increase Star Pattern in java use of for loop

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+public class Pattern1 {
+
+    public static void main(String[] args){
+        int Staring_Point;
+        int Ending_Point = 10;
+        int Staring_Point2;
+        int Ending_Point2 = 5;
+
+        for(Staring_Point = 1 ; Staring_Point < Ending_Point ; Staring_Point++){
+
+            for(Staring_Point2 = 0; Staring_Point2 < Staring_Point ; Staring_Point2++){
+
+                System.out.print("*");
+
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
